bd533219f6edacfdb82e6e73d08b1b6a2f35e59b
jamedrano <jamedrano@uvg.edu.gt>
2024-07-23 11:01:48 -0600

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -9,19 +9,19 @@ archivoModelo = st.file_uploader("Cargar Modelo")
    
 
 if archivoModelo is not None:
-    modeloprod = load_model(archivoModelo)
-    st.write("Model loaded")
-    st.write(modeloprod)
+   modeloprod = load_model(archivoModelo)
+   st.write("Model loaded")
+   st.write(modeloprod)
     
-    datosprod = st.file_uploader("Cargar Datos Prod")
-        if datosprod is not None:     
-            datospred = load_data(datosprod, 'Sheet1', 0) 
+   datosprod = st.file_uploader("Cargar Datos Prod")
+   if datosprod is not None:     
+      datospred = load_data(datosprod, 'Sheet1', 0) 
      
-             st.write("Predicting...")  
-             ypred = modeloprod.get_booster().predict(xgb.DMatrix(datospred))
-             ypred2 = pd.DataFrame({'ypred':ypred})
-             resultados = pegar(datospred, ypred2)
-             st.dataframe(resultados)
-             resulta2 = to_excel(resultados)
-             st.download_button(label='📥Descargar resultados',data=resulta2 ,file_name= 'resultados.xlsx')
+      st.write("Predicting...")  
+      ypred = modeloprod.get_booster().predict(xgb.DMatrix(datospred))
+      ypred2 = pd.DataFrame({'ypred':ypred})
+      resultados = pegar(datospred, ypred2)
+      st.dataframe(resultados)
+      resulta2 = to_excel(resultados)
+      st.download_button(label='📥Descargar resultados',data=resulta2 ,file_name= 'resultados.xlsx')
    
